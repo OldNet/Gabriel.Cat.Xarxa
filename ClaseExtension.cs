a66cf4a1276fb8396c4e3150c7c8ac367c0cc762
tetradog <tetradog@gmail.com>
2016-04-16 01:17:14 +0200

    añadido metodo sacado de internet para coger PostData en HttpListenerRequest

--- ClaseExtension.cs
+++ ClaseExtension.cs
@@ -7,28 +7,43 @@ using System.Threading.Tasks;
 
 namespace Gabriel.Cat.Extension
 {
-   public static class ClaseExtensionXarxa
-    {
-        public static void Send(this HttpListenerResponse response, string dades)
-        {
-          response.Send(Encoding.UTF8.GetBytes(dades));
-        }
-        public static void Send(this HttpListenerResponse response, byte[] dades)
-        {
-            if (!response.OutputStream.CanWrite)
-                throw new Exception("can not write on output stream!");
-            try
-            {
-                response.ContentLength64 = dades.Length;
-                response.OutputStream.Write(dades, 0, dades.Length);
-            }
-            catch { } // suppress any exceptions
-            finally
-            {
-                // always close the stream
-                response.OutputStream.Close();
-            }
+	public static class ClaseExtensionXarxa
+	{
+		public static void Send(this HttpListenerResponse response, string dades)
+		{
+			response.Send(Encoding.UTF8.GetBytes(dades));
+		}
+		public static void Send(this HttpListenerResponse response, byte[] dades)
+		{
+			if (!response.OutputStream.CanWrite)
+				throw new Exception("can not write on output stream!");
+			try {
+				response.ContentLength64 = dades.Length;
+				response.OutputStream.Write(dades, 0, dades.Length);
+			} catch {
+			} // suppress any exceptions
+            finally {
+				// always close the stream
+				response.OutputStream.Close();
+			}
 
-        }
-    }
+		}
+		public static string PostData(this HttpListenerRequest request)
+		{
+			string postData;
+			System.IO.Stream body;
+			System.IO.StreamReader reader;
+			if (!request.HasEntityBody) {
+				postData = null;
+			} else {
+				body = request.InputStream; // here we have data
+				reader = new System.IO.StreamReader(body, request.ContentEncoding);
+				postData = reader.ReadToEnd();
+				reader.Close();
+				
+			}
+			return postData;
+			
+		}
+	}
 }
